b579cad2da9721d38c78e80427fb9e1bb2361b4a
João Vítor <joovitor177@gmail.com>
2022-02-05 12:35:06 -0300

    go

--- src.go
+++ src.go
@@ -1,173 +1,132 @@
-// Click here and start typing.
+// Parte da concorrência baseada em https://gobyexample.com/waitgroups e https://go.dev/tour/concurrency/2
+// https://flaviocopes.com/go-random/
+// João Vítor Fonseca de Mendonça
+
 package main
 
 import (
 	"fmt"
-	"math"
+	"math/rand"
+	"sync"
 	"time"
 )
 
-// Exibir display de escolha para o usuário
-func display() string {
-	fmt.Println("Calculadora de Geometria Plana e Espacial")
-	fmt.Println("(1) Triângulo equilátero")
-	fmt.Println("(2) Retângulo")
-	fmt.Println("(3) Quadrado")
-	fmt.Println("(4) Círculo")
-	fmt.Println("(5) Pirâmide com base quadrangular")
-	fmt.Println("(6) Cubo")
-	fmt.Println("(7) Paralelepîpedo	")
-	fmt.Println("(8) Esfera")
-	fmt.Println("(0) Sair")
-	fmt.Print("Digite a sua opção: ")
-	//fmt.Scanln(&opcao)
-	return ""
+// Variáveis utilizadas para controle dos jogadores 1 e 2
+var wg sync.WaitGroup
+var matchpoint, pontos1, pontos2, pontosPartida, jogador1, jogador2 = 0, 0, 0, 0, "Jogador 1", "Jogador 2"
+var set = 1
+
+// Função para exibir o placar do jogo
+func exibePlacar(p1 int, p2 int) {
+	fmt.Println("-----------------------------------------------------------")
+	fmt.Println("PLACAR - ", p1, " x ", p2)
+	fmt.Println("-----------------------------------------------------------")
 }
 
-// Cálculo das medidas do triângulo
-func triangulo(base, altura float32) (float32, float32) {
-	// No menu apresentado trata-se de um triângulo equilátero
-	area := (base * altura) / 2
-	perimetro := base + altura + base
-	return area, perimetro
+// Função para cálculo do valor absoluto (fim de partida com vantagem de 2 pontos)
+func abs(x, y int) int {
+	if x < y {
+		return y - x
+	}
+	return x - y
 }
 
-// Cálculo das medidas do retângulo
-func retangulo(base, altura float32) (float32, float32) {
-	area := base * altura
-	perimetro := 2 * (base + altura)
-	return area, perimetro
-}
+// Função para controle simultâneo dos dois jogadores
+func player(nome string, vez chan bool) {
 
-// Cálculo das medidas do quadrado
-func quadrado(lado float32) (float32, float32) {
-	area := lado * lado
-	perimetro := 4 * lado
-	return area, perimetro
-}
+	// Serve para executar o Done() após a função retornar!
+	defer wg.Done()
 
-// Cálculo das medidas do círculo
-func circulo(raio float32) (float32, float32) {
-	area := (math.Pi) * raio * raio
-	perimetro := (math.Pi) * 2 * raio
-	return area, perimetro
-}
+	for {
+		// canalAberto recebe um valor booleano para controle do canal
+		canalAberto := <-vez
+		if !canalAberto {
+			fmt.Println("A partida acabou, e o Vencedor foi: ", nome)
+			fmt.Println("-----------------------------------------------------------")
+			fmt.Println("PLACAR FINAL - ", pontos1, " x ", pontos2)
+			return
+		}
+		// Gera número para decidir se vai acertar a bola ou não
+		aux := rand.Intn(4)
 
-// Cálculo das medidas da pirâmide
-func piramide(areabase, arealateral, altura float32) (float32, float32) {
-	area := areabase + arealateral
-	volume := 1 / 3 * areabase * altura
-	return area, volume
-}
+		// 75% de chance de acertar a bola
+		if aux == 0 || aux == 1 || aux == 2 {
+			fmt.Println(nome, " acertou a bola no campo adversário.")
+			// Sleep para simular o tempo da jogada do Jogador
+			//time.Sleep(1 * time.Second)
+		} else {
+			// 50% de chance de perder o ponto por não ter acertado a bola, ou jogar a bola para fora!
+			aux2 := rand.Intn(2)
+			if aux2 == 0 {
+				fmt.Println(nome, " não acertou a bola")
+			}
+			if aux2 == 1 {
+				fmt.Println(nome, " jogou para fora")
+			}
 
-// Cálculo das medidas do cubo
-func cubo(aresta float32) (float32, float32) {
-	area := 6 * aresta * aresta
-	volume := aresta * aresta * aresta
-	return area, volume
-}
+			fmt.Println("RODADA DE NÚMERO: ", set)
 
-// Cálculo das medidas do paralelepípedo
-func paralelepipedo(aresta1, aresta2, aresta3 float32) (float32, float32) {
-	area := (2 * aresta1 * aresta2) + (2 * aresta1 * aresta3) + (2 * aresta2 * aresta3)
-	volume := aresta1 * aresta2 * aresta3
-	return area, volume
-}
+			switch nome {
+			// Incrementa pontuação do Jogador 2
+			case jogador1:
+				pontos2++
+			// Incrementa pontuação do Jogador 1
+			case jogador2:
+				pontos1++
+			}
+			exibePlacar(pontos1, pontos2)
 
-// Cálculo das medidas da esfera
-func esfera(raio float32) (float32, float32) {
-	area := 4 / 3 * (math.Pi) * raio * raio
-	volume := 4 / 3 * (math.Pi) * raio * raio * raio
-	return area, volume
+			// Incrementa o número da rodada
+			set++
+		}
+		// Chegar se está na primeira rodada de Matchpoint
+		if (pontos1 == pontosPartida-1 || pontos2 == pontosPartida-1) && matchpoint == 0 {
+			fmt.Println("ATENÇÃO: PRIMEIRA RODADA DE MATCHPOINT")
+			matchpoint++
+		}
+		// Para tratar se há a vantagem de dois pontos assim que chega o valor de pontos do Sett.
+		if (abs(pontos1, pontos2) >= 2) && (pontos1 >= pontosPartida || pontos2 >= pontosPartida) {
+			// Partida acabou e o canal é fechado
+			close(vez)
+			return
+		}
+		vez <- canalAberto
+	}
 }
 
 func main() {
-	var op int
-	fmt.Println(display())
-	fmt.Scanln(&op)
-	for op != 0 {
-		//fmt.Scanln(&op)
-		switch op {
-		case 1:
-			var base, altura float32
-			// No menu apresentado trata-se de um triângulo equilátero
-			fmt.Print("Digite a base do triângulo: ")
-			fmt.Scanln(&base)
-			fmt.Print("Digite a altura do triângulo: ")
-			fmt.Scanln(&altura)
-			area, perimetro := triangulo(base, altura)
-			fmt.Println("A área do Triângulo é ", area)
-			fmt.Println("O perímetro do Triângulo é ", perimetro)
-		case 2:
-			var base, altura float32
-			fmt.Print("Digite a base do retângulo: ")
-			fmt.Scanln(&base)
-			fmt.Print("Digite a altura do retângulo: ")
-			fmt.Scanln(&altura)
-			area, perimetro := retangulo(base, altura)
-			fmt.Println("A área do Retângulo é ", area)
-			fmt.Println("O perímetro do Retângulo é ", perimetro)
-		case 3:
-			var lado float32
-			fmt.Print("Digite o lado do quadrado: ")
-			fmt.Scanln(&lado)
-			area, perimetro := quadrado(lado)
-			fmt.Println("A área do Quadrado é ", area)
-			fmt.Println("O perímetro do Quadrado é ", perimetro)
-		case 4:
-			var raio float32
-			fmt.Print("Digite o raio do círculo: ")
-			fmt.Scanln(&raio)
-			area, perimetro := circulo(raio)
-			fmt.Println("A área do Círculo é ", area)
-			fmt.Println("O perímetro do Círculo é ", perimetro)
-		case 5:
-			var areabase, arealateral, altura float32
-			fmt.Print("Digite a área da base da pirâmide: ")
-			fmt.Scanln(&areabase)
-			fmt.Print("Digite a área lateral da pirâmide: ")
-			fmt.Scanln(&arealateral)
-			fmt.Print("Digite a altura da pirâmide: ")
-			fmt.Scanln(&altura)
-			area, volume := piramide(areabase, arealateral, altura)
-			fmt.Println("A área da Pirâmide com base quadrangular é ", area)
-			fmt.Println("O volume da Pirâmide com base quadrangular é ", volume)
-		case 6:
-			var raio float32
-			fmt.Print("Digite o raio do cubo: ")
-			fmt.Scanln(&raio)
-			area, volume := cubo(raio)
-			fmt.Println("A área do Cubo é ", area)
-			fmt.Println("O volume do Cubo é ", volume)
-		case 7:
-			var aresta1, aresta2, aresta3 float32
-			fmt.Print("Digite o valor da aresta1 do paralelepípedo: ")
-			fmt.Scanln(&aresta1)
-			fmt.Print("Digite o valor da aresta2 do paralelepípedo: ")
-			fmt.Scanln(&aresta2)
-			fmt.Print("Digite o valor da aresta3 do paralelepípedo: ")
-			fmt.Scanln(&aresta3)
-			area, volume := paralelepipedo(aresta1, aresta2, aresta3)
-			fmt.Println("A área do Paralelepípedo é ", area)
-			fmt.Println("O volume do Paralelepîpedo é ", volume)
-		case 8:
-			var raio float32
-			fmt.Print("Digite o raio da esfera: ")
-			fmt.Scanln(&raio)
-			area, volume := esfera(raio)
-			fmt.Println("A área da Esfera é ", area)
-			fmt.Println("O volume da Esfera é ", volume)
-		case 0:
-			fmt.Println("Finalizando programa!")
-			break
-		default:
-			fmt.Println("Opção inválida. Escolhe uma das disponíveis!")
-		}
-		// sleep para visualizar o resultado
-		time.Sleep(2 * time.Second)
-		// imprimir menu novamente
-		fmt.Println(display())
-		fmt.Scanln(&op)
-	}
 
+	// Para garantir aleatoriedade à cada execução
+	rand.Seed(time.Now().UnixNano())
+
+	// Canal criado passando valores booleanos
+	vez := make(chan bool)
+
+	// Quantidade de pontos estabelecida
+	var quantidade int
+	fmt.Println("Digite a quantidade de pontos: ")
+	fmt.Scanln(&quantidade)
+
+	// Tratamento de erro para valores inadequados da quantidade de pontos máxima
+	for quantidade <= 0 {
+		fmt.Println("Digite uma quantidade válida, valor positivo e não nulo: ")
+		fmt.Scanln(&quantidade)
+	}
+	pontosPartida = quantidade
+
+	// Por padrão, adicionar a quantidade de Go Rotinas
+	wg.Add(2)
+
+	fmt.Println()
+	fmt.Println("Início de partida - ", jogador1, " vs ", jogador2)
+	fmt.Println()
+
+	// Go rotina para player 1
+	go player(jogador1, vez)
+	vez <- true
+	// Go rotina para player 2
+	go player(jogador2, vez)
+
+	// Wait para esperar a finalização da execução das Go rotinas
+	wg.Wait()
 }
